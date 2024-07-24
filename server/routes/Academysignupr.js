1a794e60ffd315bbf7723eccc27d892a57ea3918
AbhishekBhavnani2003 <147069739+AbhishekBhavnani2003@users.noreply.github.com>
2024-07-24 14:27:55 +0530

    Delete server directory

--- server/routes/Academysignupr.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const academyauth = require('../controllers/AcademySignupc');
-
-router.post('/academysignup', academyauth.academy_signup);
-router.post('/academylogin',academyauth.academy_login);
-
-module.exports = router;
--- server/routes/Emailr.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const emailcontroller = require('../controllers/OtpSending')
-
-router.post('/send-otp' , emailcontroller.sendotp)
-router.post('/verify-otp' , emailcontroller.verifyotp)
-
-module.exports = router;
