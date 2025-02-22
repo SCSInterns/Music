2f660fc9a6e592505666db9efb36567b28d1f798
AbhishekBhavnani2003 <abhishekbhavnani2003@gmail.com>
2025-02-22 10:46:52 +0530

    Docker-Production - 4

--- server/RedisInitalitation.js
+++ server/RedisInitalitation.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const rhost = process.env.REDIS_HOST
-const rport = process.env.REDIS_PORT
+const rport = parseInt(process.env.REDIS_PORT, 10)
 
 const redis = new Redis({
     host: rhost,
