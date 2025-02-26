930802a3179026fadddbc9bb72ac0e412d4b5ccb
AbhishekBhavnani2003 <abhishekbhavnani2003@gmail.com>
2025-02-26 17:57:39 +0530

    Bug Fix

--- server/Kafka.js
+++ server/Kafka.js
@@ -2,8 +2,8 @@ const { Kafka } = require("kafkajs")
 require('dotenv').config();
 
 const kafka = new Kafka({
-    clientId: 'my-node-app',
-    brokers: [process.env.KAFKA_BROKERS],
+    clientId: 'MusicVista-kafka',
+    brokers: ['localhost:9092'],
 })
 
 
